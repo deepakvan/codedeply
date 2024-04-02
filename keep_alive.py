41376c372af73af933092d703a489233c5cfc218
Deepak van <65806399+deepakvan@users.noreply.github.com>
2024-04-02 23:20:54 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 from threading import Thread
 import main
-is_bot_running=False
+#is_bot_running=False
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def index():
-  if not is_bot_running:
-    t = Thread(target=main.bot)
-    t.start()
-    is_bot_running=True
-    return "Bot Started"
+  #if not is_bot_running:
+  t = Thread(target=main.bot)
+  t.start()
+  #is_bot_running=True
+  #return "Bot Started"
   return "Bot Alive"
 
 
